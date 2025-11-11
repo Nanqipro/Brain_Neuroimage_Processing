9ada573846bae9af35fc71d385eec1bcb1811727
15216142617 <wenxincs@163.com>
2025-11-11 23:33:13 +0800

    1

--- CVPR/src/GCNs/run_all_experiments_multi_pub.sh
+++ CVPR/src/GCNs/run_all_experiments_multi_pub.sh
@@ -5,7 +5,8 @@
 # 并行执行：5个数据集在4个GPU上运行
 
 # 定义所有模型
-MODELS=("gcn" "gat" "sage" "gin" "chebnet" "edgeconv"  "pna" "gatv2" "deepergcn")
+# MODELS=("gcn" "gat" "sage" "gin" "chebnet" "edgeconv"  "pna" "gatv2" "deepergcn")
+MODELS=("edgeconv"  "pna" "gatv2" "deepergcn")
 # "gunet"  "hybrid" 
 # 定义数据集配置
 # 格式: "data_source:dataset:batch_size:epochs:gpu_id"
