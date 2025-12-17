f5e65f56e028b93a58c3a60a937c32923f03f8f9
xwoven <428484341@qq.com>
2025-12-17 17:52:21 +0800

    1

--- principal_neuron/src/main_emtrace_analysis.py
+++ principal_neuron/src/main_emtrace_analysis.py
@@ -761,6 +761,9 @@ if __name__ == "__main__":
     # dataset_key = None # 🔧 修改这里来指定数据集，None表示自动选择
     # dataset_key = 'emtrace01'    # 使用EMtrace01数据集
     # dataset_key = 'emtrace02'    # 使用EMtrace02数据集  
+    # dataset_key = 'emtrace01_plus'    # 使用EMtrace01增强版数据集
+    # dataset_key = 'emtrace02_plus'    # 使用EMtrace02增强版数据集
+
     # dataset_key = '2980'         # 使用2980数据集
     # dataset_key = '2980_plus'      # 使用2980增强版数据集
     # dataset_key = 'bla6250'      # 使用BLA6250数据集
@@ -769,7 +772,7 @@ if __name__ == "__main__":
     
     # dataset_key = '29800930openfield'    # 使用29800930openfield数据集
     # dataset_key = '29800924openfield'    # 使用29800924openfield数据集
-    dataset_key = '5355EM'    # 使用5355EM数据集
+    # dataset_key = '5355EM'    # 使用5355EM数据集
     
     # ===============================================================================
     # 智能数据集选择和验证
