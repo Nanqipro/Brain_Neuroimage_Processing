be273d3c8e4a8d854fa90434284302bba2163c8f
Nanqipro <2280285631@qq.com>
2026-01-06 15:54:53 +0800

    1

--- bettergcn/run_eval_datasets.sh
+++ bettergcn/run_eval_datasets.sh
@@ -5,47 +5,47 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/src"
 
 GPU_ID="${GPU_ID:-0}"
-DEFAULT_MODEL_PATH="${DEFAULT_MODEL_PATH:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/example/run_20_seed61/best_model.pth}"
+DEFAULT_MODEL_PATH="${DEFAULT_MODEL_PATH:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/example/run_20_seed61/best_model.pth}"
 
-MODEL_29800930_T0="${MODEL_29800930_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_29800930_T05="${MODEL_29800930_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_29800930_T05_LT="${MODEL_29800930_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5_反向/run_20_seed61/best_model.pth}"
+MODEL_29800930_T0="${MODEL_29800930_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_29800930_T05="${MODEL_29800930_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_29800930_T05_LT="${MODEL_29800930_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.29800930openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5_反向/run_20_seed61/best_model.pth}"
 
-MODEL_2980240924_T0="${MODEL_2980240924_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_2980240924_T05="${MODEL_2980240924_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_2980240924_T05_LT="${MODEL_2980240924_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_2980240924_T0="${MODEL_2980240924_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_2980240924_T05="${MODEL_2980240924_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_20260104_220849_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_2980240924_T05_LT="${MODEL_2980240924_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/no.2980240924openfield_CellVideo0_corrected_0_cell_trace_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_5355_T0="${MODEL_5355_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_20260105_012729_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_5355_T05="${MODEL_5355_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_20260105_012729_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_5355_T05_LT="${MODEL_5355_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_5355_T0="${MODEL_5355_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_20260105_012729_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_5355_T05="${MODEL_5355_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_20260105_012729_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_5355_T05_LT="${MODEL_5355_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_5355_3REGION_T0="${MODEL_5355_3REGION_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_20260105_012729_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_5355_3REGION_T05="${MODEL_5355_3REGION_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_20260105_012729_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_5355_3REGION_T05_LT="${MODEL_5355_3REGION_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_5355_3REGION_T0="${MODEL_5355_3REGION_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_20260105_012729_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_5355_3REGION_T05="${MODEL_5355_3REGION_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_20260105_012729_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_5355_3REGION_T05_LT="${MODEL_5355_3REGION_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/NO5355EM20251106_cell_trace-三区域_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_6250_T0="${MODEL_6250_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_20260105_071334_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_6250_T05="${MODEL_6250_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_20260105_071334_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_6250_T05_LT="${MODEL_6250_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_6250_T0="${MODEL_6250_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_20260105_071334_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_6250_T05="${MODEL_6250_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_20260105_071334_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_6250_T05_LT="${MODEL_6250_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_6250_PLUS_T0="${MODEL_6250_PLUS_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_20260105_071334_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_6250_PLUS_T05="${MODEL_6250_PLUS_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_20260105_071334_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_6250_PLUS_T05_LT="${MODEL_6250_PLUS_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_6250_PLUS_T0="${MODEL_6250_PLUS_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_20260105_071334_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_6250_PLUS_T05="${MODEL_6250_PLUS_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_20260105_071334_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_6250_PLUS_T05_LT="${MODEL_6250_PLUS_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/bla6250EM0626goodtrace_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_EMTRACE01_T0="${MODEL_EMTRACE01_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_20260105_095431_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE01_T05="${MODEL_EMTRACE01_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_20260105_095431_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE01_T05_LT="${MODEL_EMTRACE01_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_T0="${MODEL_EMTRACE01_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_20260105_095431_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_T05="${MODEL_EMTRACE01_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_20260105_095431_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_T05_LT="${MODEL_EMTRACE01_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_EMTRACE02_T0="${MODEL_EMTRACE02_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_20260105_095431_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE02_T05="${MODEL_EMTRACE02_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_20260105_095431_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE02_T05_LT="${MODEL_EMTRACE02_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_T0="${MODEL_EMTRACE02_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_20260105_095431_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_T05="${MODEL_EMTRACE02_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_20260105_095431_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_T05_LT="${MODEL_EMTRACE02_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_EMTRACE01_PLUS_T0="${MODEL_EMTRACE01_PLUS_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_20260105_140711_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE01_PLUS_T05="${MODEL_EMTRACE01_PLUS_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_20260105_140710_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE01_PLUS_T05_LT="${MODEL_EMTRACE01_PLUS_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_PLUS_T0="${MODEL_EMTRACE01_PLUS_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_20260105_140711_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_PLUS_T05="${MODEL_EMTRACE01_PLUS_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_20260105_140710_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE01_PLUS_T05_LT="${MODEL_EMTRACE01_PLUS_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace01_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
 
-MODEL_EMTRACE02_PLUS_T0="${MODEL_EMTRACE02_PLUS_T0:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_20260105_140711_阈值0.0/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE02_PLUS_T05="${MODEL_EMTRACE02_PLUS_T05:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_20260105_140711_阈值0.5/run_20_seed61/best_model.pth}"
-MODEL_EMTRACE02_PLUS_T05_LT="${MODEL_EMTRACE02_PLUS_T05_LT:-d:/GitHub_local/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_PLUS_T0="${MODEL_EMTRACE02_PLUS_T0:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_20260105_140711_阈值0.0/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_PLUS_T05="${MODEL_EMTRACE02_PLUS_T05:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_20260105_140711_阈值0.5/run_20_seed61/best_model.pth}"
+MODEL_EMTRACE02_PLUS_T05_LT="${MODEL_EMTRACE02_PLUS_T05_LT:-/home/nanqipro01/gitlocal/Brain_Neuroimage_Processing/bettergcn/results/EMtrace02_plus_thr0p5_lt/run_20_seed61/best_model.pth}"
 RUN_TS="$(date +%Y%m%d_%H%M%S)"
 OUT_ROOT="../eval_results_batch/${RUN_TS}"
 
@@ -114,9 +114,9 @@ run_eval_three \
   "../datasets/no.29800930openfield神经元编号位置图.csv" \
   "50" \
   "../datasets/effect_sizes_no.29800930openfield_CellVideo0_corrected_0_cell_trace.csv" \
-  "${MODEL_29800930_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_29800930_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_29800930_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_29800930_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_29800930_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_29800930_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 run_eval_three \
   "no.2980240924openfield_CellVideo0_corrected_0_cell_trace" \
@@ -124,9 +124,9 @@ run_eval_three \
   "../datasets/no.2980240924openfield神经元编号位置图.csv" \
   "50" \
   "../datasets/effect_sizes_no.2980240924openfield_CellVideo0_corrected_0_cell_trace.csv" \
-  "${MODEL_2980240924_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_2980240924_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_2980240924_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_2980240924_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_2980240924_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_2980240924_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 for pid in "${PIDS[@]}"; do
   wait "$pid"
@@ -142,9 +142,9 @@ run_eval_three \
   "../datasets/NO5355EM20251106_cell_trace.csv" \
   "50" \
   "../datasets/effect_sizes_NO5355EM20251106_cell_trace.csv" \
-  "${MODEL_5355_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_5355_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_5355_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_5355_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_5355_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_5355_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 run_eval_three \
   "NO5355EM20251106_cell_trace-三区域" \
@@ -152,9 +152,9 @@ run_eval_three \
   "../datasets/NO5355EM20251106_cell_trace.csv" \
   "50" \
   "../datasets/effect_sizes_NO5355EM20251106_cell_trace-三区域.csv" \
-  "${MODEL_5355_3REGION_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_5355_3REGION_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_5355_3REGION_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_5355_3REGION_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_5355_3REGION_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_5355_3REGION_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 for pid in "${PIDS[@]}"; do
   wait "$pid"
@@ -168,9 +168,9 @@ run_eval_three \
   "../datasets/6250_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_bla6250EM0626goodtrace.csv" \
-  "${MODEL_6250_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_6250_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_6250_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_6250_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_6250_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_6250_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 run_eval_three \
   "bla6250EM0626goodtrace_plus" \
@@ -178,9 +178,9 @@ run_eval_three \
   "../datasets/6250_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_bla6250EM0626goodtrace_plus.csv" \
-  "${MODEL_6250_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_6250_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_6250_PLUS_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_6250_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_6250_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_6250_PLUS_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 for pid in "${PIDS[@]}"; do
   wait "$pid"
@@ -194,9 +194,9 @@ run_eval_three \
   "../datasets/EMtrace01_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_EMtrace01.csv" \
-  "${MODEL_EMTRACE01_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE01_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE01_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_EMTRACE01_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE01_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE01_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 run_eval_three \
   "EMtrace02" \
@@ -204,9 +204,9 @@ run_eval_three \
   "../datasets/EMtrace02_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_EMtrace02.csv" \
-  "${MODEL_EMTRACE02_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE02_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE02_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_EMTRACE02_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE02_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE02_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 for pid in "${PIDS[@]}"; do
   wait "$pid"
@@ -219,9 +219,9 @@ run_eval_three \
   "../datasets/EMtrace01_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_EMtrace01_plus.csv" \
-  "${MODEL_EMTRACE01_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE01_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE01_PLUS_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_EMTRACE01_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE01_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE01_PLUS_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 run_eval_three \
   "EMtrace02_plus" \
@@ -229,9 +229,9 @@ run_eval_three \
   "../datasets/EMtrace02_Max_position.csv" \
   "50" \
   "../datasets/effect_sizes_EMtrace02_plus.csv" \
-  "${MODEL_EMTRACE02_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE02_PLUS_T05:-$DEFAULT_MODEL_PATH}" \
-  "${MODEL_EMTRACE02_PLUS_T05:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
+  "${MODEL_EMTRACE02_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE02_PLUS_T0:-$DEFAULT_MODEL_PATH}" \
+  "${MODEL_EMTRACE02_PLUS_T0:-$DEFAULT_MODEL_PATH}" & PIDS+=($!)
 
 for pid in "${PIDS[@]}"; do
   wait "$pid"
